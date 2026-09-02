95aa2be55ff2cce16e1aec1bb6e577c701f4ad51
Amisi Jospin Hassan <40860198+AmisiJospin@users.noreply.github.com>
2026-09-02 09:16:21 +0200

    Improve comments in AssemblyInfo.cs
    Updated comments for clarity in AssemblyInfo.cs.

--- Newdesign/Properties/AssemblyInfo.cs
+++ Newdesign/Properties/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+//The assembly file is more important to be used.
 [assembly: AssemblyTitle("Newdesign")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -29,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values, or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
